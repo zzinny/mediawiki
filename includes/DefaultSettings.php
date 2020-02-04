dbbfeec17d70bfc0d9bf818d082ac83afedd9ab5
Mukunda Modell <mmodell@wikimedia.org>
2020-02-04 14:44:19 -0600

    Include wmf/1.35.0-wmf.18 submodules and default settings
    Change-Id: Ia5a284c731d774f2b2c30ad91f2f1f443c350fbd

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -72,7 +72,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.35.0-alpha';
+$wgVersion = '1.35.0-wmf.18';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
